79e0b5598115aea379e33570e8c978443f94a922
Jhonatan <jhonatancaama@gmail.com>
2023-08-15 15:18:31 -0600

    Add deafultBooks.js module

---
+++ modules/defaultBooks.js
@@ -0,0 +1,4 @@
+export const defaultBooks = [
+	{ title: 'Clean Code A Handbook of Agile Software Craftsmanship', author: 'Robert C. Martin' },
+	{ title: 'JavaScript The Good Parts', author: 'Douglas Crockford' },
+];
